56f4f30237f919254471fb8be98e6cfcbd355e0d
Simon Meers <simon@simonmeers.com>
2010-10-30 09:00:33 +1100

    Make pip happy.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-form-designer',
+      version='0.1',
+      description='Admin-configurable forms',
+      author='Matthias Kestenholz',
+      author_email='mk@feinheit.ch',
+      url='http://github.com/matthiask/form_designer/',
+      packages=['form_designer'],
+      )
