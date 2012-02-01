904399f4b6f4b5ad394be045662567d40833834d
Simon Bächler <sb@feinheit.ch>
2012-02-01 11:01:17 +0100

    codec fix in export

--- form_designer/admin.py
+++ form_designer/admin.py
@@ -28,16 +28,14 @@ class UnicodeWriter:
         self.queue = cStringIO.StringIO()
         self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
         self.stream = f
+        self.encoding = encoding
         self.encoder = codecs.getincrementalencoder(encoding)()
 
     def writerow(self, row):
         row = [unicode(s) for s in row]
         self.writer.writerow([self.encoder.encode(s) for s in row])
-        # Fetch UTF-8 output from the queue ...
+        # Fetch encoded output from the queue ...
         data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
         # write to the target stream
         self.stream.write(data)
         # empty queue
