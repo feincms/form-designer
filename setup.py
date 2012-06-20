a73207bd8043f2899b773891ae54c63d3c391cca
Matthias Kestenholz <mk@spinlock.ch>
2012-06-20 20:57:19 +0200

    Better versioning and setup handling

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
+import setuplib
 
-setup(name='django-form-designer',
-      version='0.1',
-      description='Admin-configurable forms',
-      author='Matthias Kestenholz',
-      author_email='mk@feinheit.ch',
-      url='http://github.com/matthiask/form_designer/',
-      packages=['form_designer'],
-      )
+packages, package_data = setuplib.find_packages('form_designer')
+
+setup(name='FeinCMS',
+    version=__import__('form_designer').__version__,
+    description='Form Designer - a simple form designer for FeinCMS',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    author='Matthias Kestenholz',
+    author_email='mk@feinheit.ch',
+    url='http://github.com/feincms/form_designer/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    packages=packages,
+    package_data=package_data,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+    ],
+)
