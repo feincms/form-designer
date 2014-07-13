c282cdf1482827f4eca335b1fe576e15d69cdd8a
Matthias Kestenholz <mk@spinlock.ch>
2014-07-13 20:02:17 +0200

    Customize the app verbose name

--- form_designer/__init__.py
+++ form_designer/__init__.py
@@ -1,6 +1,8 @@
 VERSION = (0, 4, 0)
 __version__ = '.'.join(map(str, VERSION))
 
+default_app_config = 'form_designer.apps.FormDesignerConfig'
+
 # Do not use Django settings at module level as recommended
 try:
     from django.utils.functional import LazyObject
