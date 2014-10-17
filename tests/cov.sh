8da3ecdfddb29cad798b05cd14a47ec62ffb5898
Matthias Kestenholz <mk@spinlock.ch>
2014-10-17 17:22:04 +0200

    Fix the coverage script

--- tests/cov.sh
+++ tests/cov.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-coverage run --branch --include="*email_registration/*" --omit="*tests*" ./manage.py test testapp
+coverage run --branch --include="*form_designer/*" --omit="*tests*" ./manage.py test testapp
 coverage html
