317c63d460390036f231a142a5fe1a6cdf417e34
Matthias Kestenholz <mk@spinlock.ch>
2014-10-17 17:28:30 +0200

    Stop Django 1.7 from complaining

--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -43,3 +43,10 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 DEFAULT_FROM_EMAIL = 'no-reply@example.com'
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
