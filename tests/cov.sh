02449d40fe5294f614cc45d54def5f12f84fccf2
Matthias Kestenholz <mk@feinheit.ch>
2016-01-19 09:15:46 +0100

    Allow running cov.sh outside venv

--- tests/cov.sh
+++ tests/cov.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-coverage run --branch --include="*form_designer/*" --omit="*tests*" ./manage.py test testapp
-coverage html
+venv/bin/coverage run --branch --include="*form_designer/*" --omit="*tests*" ./manage.py test testapp
+venv/bin/coverage html
