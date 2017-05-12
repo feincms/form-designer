ed0fed7708330d69308bfa34a5af7f38a0b63531
Matthias Kestenholz <mk@feinheit.ch>
2017-05-12 19:51:22 +0200

    Verbose test running

--- tests/cov.sh
+++ tests/cov.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-venv/bin/coverage run --branch --include="*form_designer/*" --omit="*tests*" ./manage.py test testapp
+venv/bin/coverage run --branch --include="*form_designer/*" --omit="*tests*" ./manage.py test -v 2 testapp
 venv/bin/coverage html
